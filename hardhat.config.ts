0b4de1b109fb4e4799b0cefa14e006f0c7c20859
sunnyRK <radadiyasunny970@gmail.com>
2021-05-08 12:13:02 +0530

    Added github workflow badge

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,7 +28,7 @@ const config: HardhatUserConfig = {
       }
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       chainId: 1,
       accounts: {
         mnemonic: process.env.MNEMONIC,
